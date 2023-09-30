d681a9e267c7ef257d414c99c348306611cab7d9
Yura Mishin <robothot@yandex.ru>
2023-09-30 11:25:03 +0300

    Добавлена аннотация, почистил код, оптимизировал процессы

--- requests_parse.py
+++ requests_parse.py
@@ -1,41 +1,42 @@
 import time
-
 import requests
 import lxml
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime
+from collections.abc import Callable
 
 base_url = 'https://www.perekrestok.ru'
-ig = ['', 'Акции', 'Скидки', 'Готовая еда', 'Фестиваль пенного', 'От Перекрёстка', 'Молоко, сыр, яйца', 'Товары для мам и детей', 'Продукты быстрого приготовления', 'Зоотовары', 'Красота, гигиена, аптека', 'Уборка', 'Посуда', 'Для дома и дачи', 'Алкогольные напитки', 'Системы нагревания, табак']
-links_categories = ['/cat/mc/242/hleb-i-vypecka', '/cat/mc/132/maso-i-ptica', '/cat/mc/782/kolbasnye-izdelia', '/cat/mc/174/ryba', '/cat/mc/793/moreprodukty', '/cat/mc/54/zamorozennye-produkty', '/cat/mc/217/sousy-ketcupy-majonezy', '/cat/mc/79/kofe-caj-kakao-sahar', '/cat/mc/224/suhie-zavtraki-musli', '/cat/mc/74/konservacia', '/cat/mc/64/zdorove', '/cat/mc/157/orehi-semecki-suhofrukty', '/cat/mc/108/med-varene-dzemy-siropy']
+links_categories = ['/cat/mc/113/moloko-syr-ajca', '/cat/mc/148/ovosi-frukty-griby', '/cat/mc/100/makarony-krupy-maslo-specii', '/cat/mc/708/cipsy-sneki-popkorn', '/cat/mc/187/sokolad-konfety-sladosti', '/cat/mc/205/soki-vody-napitki', '/cat/mc/242/hleb-i-vypecka', '/cat/mc/132/maso-i-ptica', '/cat/mc/782/kolbasnye-izdelia', '/cat/mc/174/ryba', '/cat/mc/793/moreprodukty', '/cat/mc/54/zamorozennye-produkty', '/cat/mc/217/sousy-ketcupy-majonezy', '/cat/mc/79/kofe-caj-kakao-sahar', '/cat/mc/224/suhie-zavtraki-musli', '/cat/mc/74/konservacia', '/cat/mc/64/zdorove', '/cat/mc/157/orehi-semecki-suhofrukty', '/cat/mc/108/med-varene-dzemy-siropy']
 
 
-def timer(func):
+def timer(func: Callable) -> Callable:
+    """
+    Декоратор функции, выполняющий логирование времени работы
+    :param func: Callable
+    :return:
+    """
     def wrapper(*args, **kwargs):
-        print('_' * 30)
-        print(f'||| Запуска парсинга начался - {datetime.now()} |||')
-        print('#' * 30)
+        print(f'||| Запуска парсинга начался - {datetime.now()} |||\n{"=" * 30}')
         result = func(*args, **kwargs)
-        print('#' * 30)
-        print(f'||| Функция завершила работу - {datetime.now()} |||')
-        print('_' * 30)
+        print(f'{"=" * 30}\n||| Парсинг завершил работу - {datetime.now()} |||')
 
         return result
     return wrapper
 
 
 def get_data(url: str) -> BeautifulSoup:
-    count = 0
+    count = 1
     for _ in range(3):
-        if count:
-            print(f'Повторная попытка запроса. COUNT {count}')
+        if count > 1:
+            print(f'Повторная попытка запроса. Всего {count} из 3')
         try:
             res = requests.get(url)
             res.encoding = 'utf-8'
             doc = BeautifulSoup(res.text, 'lxml')
         except Exception as ex:
-            print(f'---!!!--- Произошла ошибка ответа сервера: {ex} ---!!!---')
+            print(f'\n---!!!--- Произошла ошибка ответа сервера ---!!!---'
+                  f'\nОписание ошибки: {ex}\n')
             time.sleep(5)
         else:
             return doc
@@ -43,57 +44,84 @@ def get_data(url: str) -> BeautifulSoup:
             count += 1
 
 
+def get_card_links(lst: list) -> None:
+    for chapter_link in lst:
+        doc = get_data(f'{base_url}{chapter_link}')
+        categories_link = [link.get('href') for link in doc.find_all('a', 'products-slider__header')]
+        for category_link in categories_link:
+            doc = get_data(f'{base_url}{category_link}')
+            products_link = [link.get('href') for link in doc.find_all('a', 'product-card__link')]
+            with open('card_links.txt', 'a', encoding='utf-8') as file:
+                for product_link in products_link:
+                    file.write(f'{product_link}\n')
+
+
 @timer
-def search_data(category_link: str) -> dict:
-    data_dict = {}
-    doc = get_data(category_link)
-    categories_link = [link.get('href') for link in doc.find_all('a', 'products-slider__header')]
-    for category_link in categories_link:
-        doc = get_data(f'{base_url}{category_link}')
-        products_link = [link.get('href') for link in doc.find_all('a', 'product-card__link')]
-        for product_link in products_link:
-            doc = get_data(f'{base_url}{product_link}')
-            categories = [cat.text for cat in doc.find_all('span', 'breadcrumb') if cat.text not in ['Главная', 'Каталог']]
+def search_data(card_links: str) -> None:
+    """
+    Ведет поиск данных, сохраняет изображение и записывает данные в json по завершению
+    :param category_link: Ожидается спиоск url
+    :return: None
+    """
+    data = {}
+
+    for card in card_links:
+        file_name_end = card_links.split('/')[-1]
+        repeat = False
+
+        for _ in range(2):
+            if repeat:
+                print('\nПовторная попытка поиска\n')
+
+            doc = get_data(f'{base_url}{card}')
+            categories = [cat.text for cat in doc.find_all('span', 'breadcrumb') if
+                          cat.text not in ['Главная', 'Каталог']]
+
             try:
                 name = doc.find('h1', 'product__title').text
+                image_url = doc.find(class_='product__gallery').find('img').get('src')
+                image = requests.get(image_url).content
                 nature_val = [val.text for val in doc.find_all('div', 'product-calories-item__value')]
                 nature_key = [key.text for key in doc.find_all('div', 'product-calories-item__title')]
                 nature_dict = dict(zip(nature_key, nature_val))
             except Exception as ex:
-                print(f'---!!!--- ОШИБКА ПОИСКА {ex} ---!!!---'
-                      f'\nСсылка {base_url}{product_link}'
-                      f'\nКод: '
-                      f'\n\tname = doc.find("h1", "product__title").text'
-                      f'\n\tnature_val = [val.text for val in doc.find_all("div", "product-calories-item__value")]'
-                      f'\n\tnature_key = [key.text for key in doc.find_all("div", "product-calories-item__title")]'
-                      f'\nЗначение без применения функций: '
-                      f'\n\t{doc.find("h1", "product__title")}'
-                      f'\n\t{doc.find_all("div", "product-calories-item__value")}'
-                      f'\n\t{doc.find_all("div", "product-calories-item__title")}')
+                print(f'---!!!--- ОШИБКА ПОИСКА ---!!!---'
+                      f'\n\tmessage: {ex}'
+                      f'\n\tlink: {base_url}{card}'
+                      f'\n\tname: {doc.find("h1", "product__title")}'
+                      f'\n\timage: {doc.find(class_="product__gallery").find("img").get("src")}'
+                      f'\n\tvalue: {doc.find_all("div", "product-calories-item__value")}'
+                      f'\n\tkey: {doc.find_all("div", "product-calories-item__title")}\n')
+                repeat = True
             else:
-                if data_dict.get(categories[-1], None):
-                    data_dict[categories[-1]][name] = nature_dict
+                if data.get(categories[-1], None):
+                    data[categories[-1]][name] = nature_dict
                 else:
-                    data_dict.setdefault(categories[-1], {name: nature_dict})
-                print(f'***Успешно получили данные {name} | {nature_dict}')
+                    data.setdefault(categories[-1], {
+                        name: nature_dict,
+                        'image_name': f'img_{file_name_end}.jpg'
+                    })
+                print(f'+ Товар {name} | {nature_dict}')
 
-    return data_dict
+                with open(f'images/img_{file_name_end}.jpg', 'wb') as img:
+                    img.write(image)
+                print(f'+ Изображение img_{file_name_end}.jpg')
+                break
 
-
-num_file = 6
-for link in links_categories:
-    data = search_data(f'{base_url}{link}')
-    with open(f'rec{num_file}.json', 'w', encoding='utf-8') as file:
+    with open(f'files/data.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
-        print(print('=' * 30))
-        print('Запись файла rec{num_file}.json прошла успешно.'
-              '\nВремя: {time}'
-              '\n{link_category}'.format(
-            num_file=num_file,
-            time=datetime.now(),
-            link_category=link
-        )
-              )
-        print(print('=' * 30))
-    num_file += 1
+
+
+def main():
+    get_card_links(links_categories)
+    with open('card_links.txt', 'r', encoding='utf-8') as file:
+        search_data(file.readlines())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
--- selenium_parse.py
+++ selenium_parse.py
@@ -1,27 +1,36 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-user_agent = "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
+user_agent = "Your User-Agent"#user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0
+browser_driver_path = 'Path webdriver'#C:/Users/Босс/Desktop/prog/PycharmProjects/parser/firefoxdriver/geckodriver.exe
+
+
+def settings_options(path: str):
+    sv = Service(executable_path=path)
+    if path.endswith('chromedriver.exe'):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument(f"--user-agent={user_agent}")
+        driver = webdriver.Chrome(
+            service=sv,
+            options=options
+        )
+        return driver
+    elif path.endswith('geckodriver.exe'):
+        options = webdriver.FirefoxOptions()
+        options.set_preference('dom.webdriver.enabled', False)
+        options.set_preference('general.useragent.override', user_agent)
+        driver = webdriver.Firefox(
+            service=sv,
+            options=options
+        )
+        return driver
 
 
 def get_data(url):
-    print('Запускаем функцию')
-    options = webdriver.FirefoxOptions()
-    options.set_preference('dom.webdriver.enabled', False)
-    options.set_preference('general.useragent.override', user_agent)
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-    # options.add_argument("user-agent=AutomationControlled")
-
-    sv = Service(executable_path='C:/Users/Босс/Desktop/prog/PycharmProjects/parse_selenium/geckodriver.exe')
-    driver = webdriver.Firefox(
-        service=sv,
-        options=options
-    )
-    # driver.get(url=url)
-
+    driver = settings_options(browser_driver_path)
     try:
         print(f'Запрашиваем {url} у браузера')
         driver.get(url=url)
@@ -34,9 +43,8 @@ def get_data(url):
     finally:
         driver.close()
         driver.quit()
-    print('Завершаем функцию')
 
 #https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html
 # https://www.auchan.ru/catalog/
 # https://n5m.ru/usagent.html
-get_data('https://www.auchan.ru/catalog/')
+get_data('https://n5m.ru/usagent.html')
