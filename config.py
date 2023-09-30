f2ce01958b96d919992e7a8cb9c9338ac9579b2e
Yura Mishin <robothot@yandex.ru>
2023-09-30 11:31:34 +0300

    Добавлен config

---
+++ config.py
@@ -0,0 +1,22 @@
+base_url = 'https://www.perekrestok.ru'
+links_categories = [
+    '/cat/mc/113/moloko-syr-ajca',
+    '/cat/mc/148/ovosi-frukty-griby',
+    '/cat/mc/100/makarony-krupy-maslo-specii',
+    '/cat/mc/708/cipsy-sneki-popkorn',
+    '/cat/mc/187/sokolad-konfety-sladosti',
+    '/cat/mc/205/soki-vody-napitki',
+    '/cat/mc/242/hleb-i-vypecka',
+    '/cat/mc/132/maso-i-ptica',
+    '/cat/mc/782/kolbasnye-izdelia',
+    '/cat/mc/174/ryba',
+    '/cat/mc/793/moreprodukty',
+    '/cat/mc/54/zamorozennye-produkty',
+    '/cat/mc/217/sousy-ketcupy-majonezy',
+    '/cat/mc/79/kofe-caj-kakao-sahar',
+    '/cat/mc/224/suhie-zavtraki-musli',
+    '/cat/mc/74/konservacia',
+    '/cat/mc/64/zdorove',
+    '/cat/mc/157/orehi-semecki-suhofrukty',
+    '/cat/mc/108/med-varene-dzemy-siropy'
+]
